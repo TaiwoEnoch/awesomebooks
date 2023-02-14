2944e6d716f4bfdd9bf7168b789fe8610d570717
Reggeditt <tkodjosarso@gmail.com>
2023-02-14 17:58:31 +0000

    update

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const authorInputEl = document.getElementById('author-input');
 const formEl = document.getElementById('form');
 const collectionsSectionEl = document.getElementById('books-collection');
 
-const removeButtons = [];
-
 class BookCollection {
   constructor() {
     this.collectionData = JSON.parse(window.localStorage.getItem('collectionArray')) || [];
+    this.removeButtons = [];
   }
 
   addBook(book) {
@@ -33,20 +32,18 @@ const renderBooks = (data) => {
   data.forEach((book, index) => {
     const bookEntry = document.createElement('div');
     bookEntry.classList.add('book-entry');
-    const bookTitle = document.createElement('p');
-    bookTitle.classList.add('book-title');
-    bookTitle.textContent = book.title;
-    const bookAuthor = document.createElement('p');
-    bookAuthor.classList.add('book-author');
-    bookAuthor.textContent = book.author;
-    removeButtons[index] = document.createElement('button');
-    removeButtons[index].classList.add('remove-button');
-    removeButtons[index].textContent = 'Remove';
-    removeButtons[index].dataset.id = book.bookID;
-    bookEntry.append(bookTitle, bookAuthor, removeButtons[index]);
+    const bookInfo = document.createElement('span');
+    bookInfo.classList.add('book-info');
+    bookInfo.textContent = `"${book.title}" by ${book.author}`;
+    bookCollection.removeButtons[index] = document.createElement('button');
+    bookCollection.removeButtons[index].classList.add('remove-button');
+    bookCollection.removeButtons[index].textContent = 'Remove';
+    bookCollection.removeButtons[index].dataset.id = book.bookID;
+    bookEntry.append(bookInfo, bookCollection.removeButtons[index]);
     collectionsSectionEl.appendChild(bookEntry);
   });
-  removeButtons.forEach((button) => {
+
+  bookCollection.removeButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
       bookCollection.removeBook(+e.target.dataset.id);
       renderBooks(bookCollection.collectionData);
@@ -64,15 +61,4 @@ formEl.addEventListener('submit', (e) => {
   renderBooks(bookCollection.collectionData);
 });
 
-// const removeBook = (id) => {
-//   collectionData = collectionData.filter((book) => book.bookID !== id);
-//   collectionData.forEach((book, index) => { book.bookID = index; });
-//   renderBooks(collectionData);
-//   window.localStorage.setItem('collectionArray', JSON.stringify(collectionData));
-// };
-
-// removeButtons.forEach((button) => {
-//   console.log(button);
-// });
-
-// renderBooks(bookCollection);
+renderBooks(bookCollection.collectionData);
