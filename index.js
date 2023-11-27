2c3561d1557bfec000aea5eda810f0863ec2c88d
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-11-27 13:23:56 +0100

    Add comments

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+// Import the bookCollection instance and renderBooks function from their respective modules
 import bookCollection from './module/bookCollection.js';
 import renderBooks from './module/renderBook.js';
+
+// Import the DateTime class from the Luxon library for date and time handling
 import { DateTime } from './luxon.js';
 
+// Get references to various HTML elements using their IDs or classes
 const titleInputEl = document.getElementById('title-input');
 const authorInputEl = document.getElementById('author-input');
 const formEl = document.getElementById('form');
@@ -9,21 +13,32 @@ const contactSection = document.querySelector('.contact-section');
 const collectionsSectionEl = document.getElementById('books-collection');
 const bookSectionEl = document.getElementById('books-section');
 
+// Event listener for the form submission to add a new book to the collection
 formEl.addEventListener('submit', (e) => {
+  // Prevent the default form submission behavior
   e.preventDefault();
+
+  // Retrieve the values of the title and author input fields
   const title = titleInputEl.value;
   const author = authorInputEl.value;
+
+  // Create a book object with the input values and a unique bookID
   const removeIndex = bookCollection.collectionData.length;
   const book = { title, author, bookID: removeIndex };
+
+  // Add the new book to the collection and re-render the books
   bookCollection.addBook(book);
   renderBooks(collectionsSectionEl, bookCollection);
+
+  // Clear the input fields after adding the book
   titleInputEl.value = '';
   authorInputEl.value = '';
 });
 
+// Initial rendering of the books when the page loads
 renderBooks(collectionsSectionEl, bookCollection);
 
-// display the books list when click the button "List"
+// Event listener to display the books list when the "List" button is clicked
 const listBtn = document.getElementById('list-books');
 listBtn.addEventListener('click', () => {
   bookSectionEl.style.display = 'block';
@@ -31,7 +46,7 @@ listBtn.addEventListener('click', () => {
   contactSection.style.display = 'none';
 });
 
-// display the Add book form  when click the button "Add new"
+// Event listener to display the Add book form when the "Add new" button is clicked
 const addNewBtn = document.querySelector('.add-new-btn');
 addNewBtn.addEventListener('click', () => {
   formEl.style.display = 'flex';
@@ -39,7 +54,7 @@ addNewBtn.addEventListener('click', () => {
   bookSectionEl.style.display = 'none';
 });
 
-// display the  Contact section when click the button "Contact"
+// Event listener to display the Contact section when the "Contact" button is clicked
 const contactBtn = document.querySelector('.contact');
 contactBtn.addEventListener('click', () => {
   contactSection.style.display = 'flex';
@@ -47,8 +62,11 @@ contactBtn.addEventListener('click', () => {
   bookSectionEl.style.display = 'none';
 });
 
+// Update the current date and time every second using Luxon's DateTime class
 setInterval(() => {
   const now = DateTime.now();
   const date = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+
+  // Update the HTML element with the current date and time
   document.getElementById('set-date').innerHTML = date;
 }, 1000);
